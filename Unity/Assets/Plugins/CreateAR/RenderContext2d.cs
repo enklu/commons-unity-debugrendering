99df5146930c0090ee844425e8da3ac87955fb0e
Benjamin Jordan <bmj465@gmail.com>
2017-08-28 12:58:28 -0700

    More comments.

--- Unity/Assets/Plugins/CreateAR/RenderContext2d.cs
+++ Unity/Assets/Plugins/CreateAR/RenderContext2d.cs
@@ -2,21 +2,36 @@
 
 namespace CreateAR.Commons.Unity.DebugRenderer
 {
-    public class RenderContext2DInternal : RenderContext
-    {
-        protected override void Setup(int mode)
-        {
-            GL.PushMatrix();
-            GL.LoadPixelMatrix();
-            GL.Begin(mode);
-            GL.Color(_color);
-        }
-    }
-
+    /// <summary>
+    /// An object that actually makes the GL calls for 2D primitives.
+    /// </summary>
     public class RenderContext2D
     {
+        /// <summary>
+        /// A regular RenderContext we prefix with a different matrix.
+        /// </summary>
+        private class RenderContext2DInternal : RenderContext
+        {
+            /// <inheritdoc cref="RenderContext"/>
+            protected override void Setup(int mode)
+            {
+                GL.PushMatrix();
+                GL.LoadPixelMatrix();
+                GL.Begin(mode);
+                GL.Color(_color);
+            }
+        }
+
+        /// <summary>
+        /// The internal context to draw with.
+        /// </summary>
         private readonly RenderContext2DInternal _context = new RenderContext2DInternal();
 
+        /// <summary>
+        /// Sets the color of primitives.
+        /// </summary>
+        /// <param name="color">The color to set.</param>
+        /// <returns></returns>
         public RenderContext2D Color(Color color)
         {
             _context.Color(color);
@@ -24,6 +39,12 @@ namespace CreateAR.Commons.Unity.DebugRenderer
             return this;
         }
 
+        /// <summary>
+        /// Draws a line segment.
+        /// </summary>
+        /// <param name="from">Starting point.</param>
+        /// <param name="to">Ending point.</param>
+        /// <returns></returns>
         public RenderContext2D Line(Vector2 from, Vector2 to)
         {
             _context.Line(from, to);
@@ -31,6 +52,11 @@ namespace CreateAR.Commons.Unity.DebugRenderer
             return this;
         }
 
+        /// <summary>
+        /// Draws a list of line segments.
+        /// </summary>
+        /// <param name="lines">List of pairs of start, end points.</param>
+        /// <returns></returns>
         public RenderContext2D Lines(Vector2[] lines)
         {
             var len = lines.Length;
@@ -45,6 +71,12 @@ namespace CreateAR.Commons.Unity.DebugRenderer
             return this;
         }
 
+        /// <summary>
+        /// Draws a square.
+        /// </summary>
+        /// <param name="center">Center of the square.</param>
+        /// <param name="size">The size of the square.</param>
+        /// <returns></returns>
         public RenderContext2D Square(Vector2 center, float size)
         {
             return Rectangle(new Rect(
@@ -54,6 +86,11 @@ namespace CreateAR.Commons.Unity.DebugRenderer
                 size));
         }
 
+        /// <summary>
+        /// Draws a rectangle.
+        /// </summary>
+        /// <param name="rect">The rect to draw.</param>
+        /// <returns></returns>
         public RenderContext2D Rectangle(Rect rect)
         {
             _context.LineStrip(new[]
